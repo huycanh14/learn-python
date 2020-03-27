6ce197a4d0553dcf063a7d5c629da28e82716d69
Huy Canh <huycanh14@gmail.com>
2020-03-27 10:30:14 +0700

    options print

---
+++ print.py
@@ -0,0 +1,10 @@
+# print('hi' + ' ' + 'all')
+a = 33
+f = 12.5
+name = 'kain'
+# print('The number is ' + str(f))
+# print('The number is %d' % a)
+print('The number is %d and %f' % (a, f))
+print('hi %s' % name)
+print('The numbers is {} and {} the name is {}'.format(a, f, name))
+print(f'The number is {a} and {f} and the name is {name}')
