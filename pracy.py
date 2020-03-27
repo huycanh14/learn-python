ab2f379001b3836c8c785331b4efb5778b603046
Huy Canh <huycanh14@gmail.com>
2020-03-27 09:50:52 +0700

    list introduction andd how to insert, pop, remove, append

---
+++ pracy.py
@@ -0,0 +1,21 @@
+list_1 = ['pedram', 'ali', 'john', 'maryam']
+
+# print(list_1[0])
+# a = list_1[0]
+# b = list_1[1]
+# c = list_1[2]
+# d = list_1[3]
+# print(a, b, c, d)
+
+# a = a.title()
+# b = list_1[-1]
+# c = list_1[-2]
+# print(c)
+
+# print(len(list_1))
+list_1.append('car') #insert data in end list
+list_1.insert(3, 'car') #inser data in any position
+list_1.remove('car') #remove by first name search
+a = list_1.pop(2) #get data in positon assign value to variable and delete value from list
+print(list_1)
+print(a)
