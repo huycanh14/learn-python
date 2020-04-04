8f4cd36d0283c709d481d2be1eda46a6a75be4bb
Huy Canh <huycanh14@gmail.com>
2020-04-04 18:54:12 +0700

    make and use master page

--- mywebsite/main/urls.py
+++ mywebsite/main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^home/$', views.home, name = 'home')
+    url(r'^$', views.home, name = 'home'),
+    url(r'^about/$', views.about, name = 'about')
 ]
 
