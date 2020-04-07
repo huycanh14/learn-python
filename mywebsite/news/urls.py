8621fd7d9db085b4e94701403dcf2ce9a2cf5341
Huy Canh <huycanh14@gmail.com>
2020-04-07 19:37:10 +0700

    new app

--- mywebsite/news/urls.py
+++ mywebsite/news/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    
+    url(r'^news/(?P<pk>\d+)/$', views.news_detail, name='news_detail')
+    # url(r'^news/(?P<word>\d+)/$', views.news_detail, name='news_detail')
+    # url(r'^news/(?P<word>.*)/$', views.news_detail, name='news_detail')
+
 ]
--- mywebsite/news/views.py
+++ mywebsite/news/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import News
+from main.models import Main
 
 # Create your views here.
+
+def news_detail(request, name):
+    # news = News.objects.filter(pk=pk)
+    news = News.objects.filter(name=word)
+    site = Main.objects.get(pk=3)
+    return render(request, 'front/news_detail.html', {'news': news, 'site': site})
