542aaae4e9d3dc1568d8788b9e8c17974e530cfc
Huy Canh <huycanh14@gmail.com>
2020-04-08 13:07:14 +0700

    update file

--- mywebsite/news/views.py
+++ mywebsite/news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import News
 from main.models import Main
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 
@@ -27,9 +28,26 @@ def news_add(request):
         if newstitle == '' or newscat == '' or newscat == '' or newstxt == '':
             error = "All Fields Requirded"
             return render(request, 'back/error.html', {'error': error})
+        try:
+            myfile = request.FILES['myfile']
+            fs = FileSystemStorage()
+            filename = fs.save(myfile.name, myfile)
+            url = fs.url(filename)
+            if str(myfile.content_type).startswith('image'):
+                if myfile.size < 5000000:
+                    b = News(name=newstitle, short_txt=newstxtshort, body_txt=newstxt, date="2020", picname = filename, picurl=url, writer='-', catname=newscat, catid=0, show=0)
+                    b.save()
+                    return redirect('news_list')
+                else:
+                    error = "Your File Is Bigger Than 5 MB"
+                    return render(request, 'back/error.html', {'error': error})
+            else:
+                error = "Your File Not Supported"
+                return render(request, 'back/error.html', {'error': error})
 
-        b = News(name=newstitle, short_txt=newstxtshort, body_txt=newstxt, date="2020", pic="-", writer='-', catname=newscat, catid=0, show=0)
-        b.save()
-        return redirect('news_list')
+        except:
+            error = "Please Input Your Image"
+            return render(request, 'back/error.html', {'error': error})
+
     return render(request, 'back/news_add.html')
 
