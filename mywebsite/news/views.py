573adeeed706c235275dc75a55e4e6a40c53016e
Huy Canh <huycanh14@gmail.com>
2020-04-08 09:59:37 +0700

    make add news page

--- mywebsite/news/views.py
+++ mywebsite/news/views.py
@@ -15,3 +15,21 @@ def news_detail(request, word):
 def news_list(request):
     news = News.objects.all()
     return render(request, 'back/news_list.html', {'news': news})
+
+
+def news_add(request):
+    if request.method == 'POST':
+        newstitle = request.POST.get('newstitle')
+        newscat = request.POST.get('newscat')
+        newstxtshort = request.POST.get('newstxtshort')
+        newstxt = request.POST.get('newstxt')
+
+        if newstitle == '' or newscat == '' or newscat == '' or newstxt == '':
+            error = "All Fields Requirded"
+            return render(request, 'back/error.html', {'error': error})
+
+        b = News(name=newstitle, short_txt=newstxtshort, body_txt=newstxt, date="2020", pic="-", writer='-', catname=newscat, catid=0, show=0)
+        b.save()
+        return redirect('news_list')
+    return render(request, 'back/news_add.html')
+
