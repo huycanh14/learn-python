d199ef542a5f9c811b14bf35ecb5607a15754922
Huy Canh <huycanh14@gmail.com>
2020-04-08 12:26:51 +0700

    setting

--- mywebsite/mywebsite/urls.py
+++ mywebsite/mywebsite/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
+# from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
@@ -8,3 +10,7 @@ urlpatterns = [
     url(r'', include('main.urls')),
     url(r'', include('news.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
